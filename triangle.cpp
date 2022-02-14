416af93361cd2f5a7497b6647d963be4ac266dc5
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-02-14 13:57:53 -0500

    basic hello window program complete

--- triangle.cpp
+++ triangle.cpp
@@ -5,7 +5,15 @@
 
 #include "constants.h"
 
-int main() {
+// create callback function which gets called whenever the window gets
+// resized
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+int main()
+{
 	glfwInit();
 
 	// set the window hints to reflect version
@@ -16,7 +24,8 @@ int main() {
 	
 	// create GLFW window object
 	// not really sure what the nulls are for
-	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "OpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT,
+            "OpenGL", NULL, NULL);
 
 	// catch initialization failure and cleanup
 	if (window == NULL)
@@ -30,8 +39,8 @@ int main() {
 	glfwMakeContextCurrent(window);
 	
 	// now initialize GLAD
-	// glfwGetProcAddress accounts for which OS we are compiling for and gives the correct
-	// function for loading GLAD
+	// glfwGetProcAddress accounts for which OS we are compiling for and gives
+    // the correct function for loading GLAD
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 	    std::cout << "Failed to initialize GLAD" << std::endl;
@@ -39,8 +48,26 @@ int main() {
 	}
 
 	// our first openGL function
-	// tells opengl the size of the rendering window (make it the same size as the window glfw just created)
+	// tells opengl the size of the rendering window (make it the same size as
+    // the window glfw just created)
 	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    
+    // tell glfw to bind that func to resizing the window
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    // enter window loop
+    while(!glfwWindowShouldClose(window))
+    {
+        // moves the buffer that is being drawn to into the window so it
+        // actually gets drawn by the graphical environment
+        glfwSwapBuffers(window);
+
+        // checks if any events like window resizing or button presses happen
+        glfwPollEvents();    
+    }
+    
+    // cleanup glfw's allocated resources
+    glfwTerminate();
 
 	return 0;
 }
