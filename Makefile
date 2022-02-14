c6a4ca5fde7105d7384e5b6ee44ad11c6f991e1c
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-02-14 12:20:41 -0500

    fixed makefile to include glad.c

--- Makefile
+++ Makefile
@@ -4,17 +4,17 @@ CC = g++
 # compiler flags:
 #  -g     - this flag adds debugging information to the executable file
 #  -Wall  - this flag is used to turn on most compiler warnings
-CFLAGS  = -lglfw3 -lGL -lX11 -lpthread -lXrandr -lXi -ldl -I include/
+CFLAGS  = -g -Wall -lglfw3 -lGL -lX11 -lpthread -lXrandr -lXi -ldl -I ./include/
 
 # The build target 
 TARGET = triangle
 
+LINKED = glad.c
+
 all: $(TARGET).o
 
 $(TARGET).o: $(TARGET).cpp
-	# CFLAGS MUST COME AFTER TARGET FOR CORRECT LINKING
-	#  :(
-	$(CC) -o $(TARGET).o $(TARGET).cpp $(CFLAGS)
+	$(CC) -o $(TARGET).o $(TARGET).cpp $(LINKED) $(CFLAGS)
 
 clean:
-	$(RM) $(TARGET)
+	$(RM) $(TARGET).o
--- triangle.cpp
+++ triangle.cpp
@@ -4,12 +4,41 @@
 #include <iostream>
 
 int main() {
-    glfwInit();
+	glfwInit();
 
-    // set the window hints to reflect version
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// set the window hints to reflect version
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    return 0;
+	
+	// create GLFW window object
+	// not really sure what the nulls are for
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+
+	// catch initialization failure and cleanup
+	if (window == NULL)
+	{
+	    std::cout << "Failed to create GLFW window" << std::endl;
+	    glfwTerminate();
+	    return -1;
+	}
+
+	// if success, set the current opengl context to be this window
+	glfwMakeContextCurrent(window);
+	
+	// now initialize GLAD
+	// glfwGetProcAddress accounts for which OS we are compiling for and gives the correct
+	// function for loading GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+	    std::cout << "Failed to initialize GLAD" << std::endl;
+	    return -1;
+	}
+
+	// our first openGL function
+	// tells opengl the size of the rendering window (make it the same size as the window glfw just created)
+	glViewport(0, 0, 800, 600);
+
+	return 0;
 }
