111c31d02673454379533c3e6209c7f09d8208de
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-03-08 15:18:54 -0600

    updated headers

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
-#include <glad/glad.h> 
-#include <GLFW/glfw3.h>
-#include "stb_image.h"
-
 #include <iostream>
 #include <cmath>
+
+#include <glad/glad.h> 
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "stb_image.h"
 
 #include "constants.h"
 #include "shader.h"
