57cbca5d9d98a95741898a3cf56871a0f7d61c48
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-05-15 10:17:54 -0500

    completed opengl boilerplate

--- include/constants.h
+++ include/constants.h
@@ -1,21 +1,6 @@
 #pragma once
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
-#define WINDOW_TITLE "opengl demo"
+#define WINDOW_WIDTH 1000
+#define WINDOW_HEIGHT 1000
+#define WINDOW_TITLE "lighting demo"
 
-#define VERTEX_PASSTHROUGH "shaders/passthrough.vs"
-#define FRAG_YELLOW "shaders/yellow.fs"
-#define FRAG_TIMER "shaders/time.fs"
-#define FRAG_COL_POS "shaders/poscolor.fs"
-#define VERTEX_COL_POS "shaders/poscolor.vs"
-#define VERTEX_OFFSET "shaders/offset.vs"
-#define VERTEX_TEX_OFFSET "shaders/texture-offset.vs"
-#define FRAG_TEX "shaders/texture.fs"
-#define FRAG_FULL "shaders/full.fs"
-#define VERTEX_FULL "shaders/full.vs"
-#define FRAG_TIME_TEXTURED "shaders/time-textured.fs"
-
-// 3d shaders!!! (they use da matrices for transform)
-#define VERTEX_FULL_3D "shaders/full-3d.vs"
-#define VERTEX_TEXTURE_3D "shaders/nocolor-3d.vs"
--- main.cpp
+++ main.cpp
@@ -1,6 +1,49 @@
+#include <iostream>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-int main()
-{
-    return 0;
+#include "constants.h"
+
+// function called whenever a user resizes the window
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+int main() {
+  // initialize glfw
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // create a window using glfw
+  GLFWwindow *window =
+      glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
+
+  // handle failure
+  if (window == NULL) {
+    std::cout << "Failed to initialize GLFW Window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  // tell opengl the size of the viewport, which we just created with GLFW
+  glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+  // window's update loop
+  while (!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
 }
