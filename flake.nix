4ea32524e2b91aa2f724d2820358eaa5e039cbfb
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-06-04 16:10:35 -0800

    flake actually working now, needs glfw

--- flake.nix
+++ flake.nix
@@ -3,22 +3,40 @@
   description = "openGL practice project";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-21.11";
-    #nixpkgs.url = "nixpkgs/nixos-unstable";
+    #nixpkgs.url = "nixpkgs/nixos-21.11";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
     let
-      system = "${builtins.currentSystem}";
+        system = "x86_64-linux";
+        pkgs = import nixpkgs { inherit system; };
     in
     {
+        #defaultPackage.${system} = 
+        #stdenv.mkDerivation {
+        #    name = "glportals";
+        #    src = self;
+        #    buildPhase = "python build.py";
+        #    # installPhase = "...
+        #};
         devShell.${system} =
-            nixpkgs.mkShell { 
-                nativeBuildInputs = with nixpkgs; [
+            pkgs.mkShell {
+                shellHoook = ''
+alias build=python build.py
+# get access to neovim while in devshell
+alias nvim=${pkgs.neovim}/bin/nvim
+                '';
+                nativeBuildInputs = with pkgs; [
                     gcc
-                    python
+                    python3Minimal
                     bear
-                    glfw
+
+                    # libraries
+                    glfw-wayland
+                    xorg.libXrandr
+                    xorg.libXi
+                    xorg.libX11
                 ];
             };
     };
