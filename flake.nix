0007347299be3be4f4c2c5ea49419b187349e064
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-06-18 10:48:27 -0500

    format

--- flake.nix
+++ flake.nix
@@ -9,35 +9,35 @@
 
   outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs:
     let
-        system = "x86_64-linux";
-        pkgs = import nixpkgs { inherit system; };
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
       unstable = nixpkgs-unstable.legacyPackages.${system};
     in
     {
-        #defaultPackage.${system} = 
-        #stdenv.mkDerivation {
-        #    name = "glportals";
-        #    src = self;
-        #    buildPhase = "python build.py";
-        #    # installPhase = "...
-        #};
-        devShell.${system} =
-            pkgs.mkShell {
-                shellHook = ''
-alias build="python build.py"
-                '';
-                nativeBuildInputs = with pkgs; [
-                    unstable.neovim
-                    gcc
-                    python3Minimal
-                    bear
+      #defaultPackage.${system} = 
+      #stdenv.mkDerivation {
+      #    name = "glportals";
+      #    src = self;
+      #    buildPhase = "python build.py";
+      #    # installPhase = "...
+      #};
+      devShell.${system} =
+        pkgs.mkShell {
+          shellHook = ''
+            alias build="python build.py"
+          '';
+          nativeBuildInputs = with pkgs; [
+            unstable.neovim
+            gcc
+            python3Minimal
+            bear
 
-                    # libraries
-                    glfw3
-                    xorg.libXrandr
-                    xorg.libXi
-                    xorg.libX11
-                ];
-            };
+            # libraries
+            glfw3
+            xorg.libXrandr
+            xorg.libXi
+            xorg.libX11
+          ];
+        };
     };
 }
