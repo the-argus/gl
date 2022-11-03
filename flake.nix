4808ee863a0ac8511e503b9c7d267ff33c1a690a
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-11-03 10:47:05 -0400

    fixed clang environment

--- flake.nix
+++ flake.nix
@@ -5,36 +5,35 @@
     nixpkgs.url = "nixpkgs/nixos-22.05";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      llvm = pkgs.llvmPackages_latest;
-    in
-    {
-      devShell.${system} = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } rec {
-        nativeBuildInputs = with pkgs; [
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+    llvm = pkgs.llvmPackages_latest;
+  in {
+    devShell.${system} =
+      pkgs.mkShell.override {
+        stdenv = pkgs.clangStdenv;
+      } rec {
+        packages = with pkgs; [
           python3Minimal
           bear
-          clang
           clang-tools
 
           # debugger
           llvm.lldb
           gdb
 
-          # lsp and std lib
-          llvm.libstdcxxClang
-
           # libraries
           assimp
-          llvm.libcxx
-          llvm.libllvm
           glfw3
           xorg.libXrandr
           xorg.libXi
           xorg.libX11
         ];
       };
-    };
+  };
 }
